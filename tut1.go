b028d850d99e2cc6d345f73f3f0f5cf8041a8421
Aqsa Fatima <aqsafatima0202@gmail.com>
2023-10-27 10:30:32 +0500

    2 files added

---
+++ tut1.go
@@ -0,0 +1,6 @@
+package main
+import "fmt"
+func main(){
+    fmt.Println("Hello,World")
+    fmt.Println("This is the second Line")
+}
---
+++ tut2.go
@@ -0,0 +1,10 @@
+package main
+import "fmt"
+
+func main(){
+	fmt.Println("This is the tutorial for Variables:")
+
+	var aqsa int = 21
+
+	fmt.Println("aqsa = " ,aqsa)
+}
